38bfdf6acfdcab810c30d81f52bb2ecbe715870b
Zachary Norton <zanorton@ncsu.edu>
2025-12-18 17:20:10 -0500

    added fe api directories, basic backend

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// HOME
+app.get('/', (req, res) => {
+    res.send("Hello world")
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
