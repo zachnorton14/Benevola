e11c4042f7da7daff5781f7fae5a7fac83ac8461
Zachary Norton <zanorton@ncsu.edu>
2025-12-22 13:51:57 -0500

    restructured merged code. removed node_modules

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const sequelize = require('./src/database');
+const sequelize = require('./src/db/database');
 const PORT = process.env.PORT || 3000;
 
 const eventsRouter = require('./src/routes/events');
--- api/src/models/Event.js
+++ api/src/models/Event.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database');
+const sequelize = require('../db/database');
 
 const Event = sequelize.define('Event', {
     id: {
