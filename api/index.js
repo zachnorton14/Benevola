6b0c67261ee9fbda54578fc921bd30b2a0a67657
tdjibri <tdjibri@ncsu.edu>
2025-12-21 20:46:53 -0500

    Added the GET API Call just to try it out

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,3 +11,46 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+//Connects to the database file
+const db = new sqlite3.Database('./BenevaolaDB', (err) => {
+  if (err) {
+    console.error("There was an error opening the database: ", err.message);
+  } else {
+    console.log("Successfully connected to the SQLite Database.");
+  }
+  // This creates the table if it's missing
+        db.run(`CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            role TEXT
+        )`, (err) => {
+            if (err) {
+                console.error("Table creation failed:", err.message);
+            } else {
+                console.log("Users table is ready.");
+            }
+        });
+});
+
+app.use(express.json()); //Lets the API read JSON data sent to it
+
+//Okay trying to implement a GET route
+app.get('/api/users', (req, res) => {
+    const sql = "SELECT * FROM users";
+    
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running at http://localhost:${port}')
+})
