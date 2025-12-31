79e69fc805d2bd5542935bc5670b0c5d3c5a851b
tdjibri <tdjibri@ncsu.edu>
2025-12-30 22:07:44 -0500

    Updated org to work with events where we can get events associated with the organzation

--- api/src/routes/organizations.js
+++ api/src/routes/organizations.js
@@ -24,6 +24,7 @@ router.post('/', async (req, res) => {
     }
 });
 
+// GET organizations
 router.get('/', async (req, res) => {
     try {
         const orgs = await Organization.findAll();
@@ -57,6 +58,115 @@ router.get('/:oid', async (req, res) => {
         res.status(500).json({ findError: err.message });
     }
 });
+// REPLACE an event
+router.put('/:oid', async (req, res) => {
+    try {
+        // validate id route param
+        const validatedId = orgParamsValidation.safeParse(req.params);
+        if (!validatedId.success) return res.status(400).json({ paramValidationError: validatedId.error.issues });
+
+        const oid = validatedId.data.oid;
+
+        // validate the request body
+        const validatedBody = orgValidation.safeParse(req.body);
+        if (!validatedBody.success) return res.status(400).json({ bodyValidationError: validatedBody.error.issues });
+
+        const org = await Organization.findByPk(oid);
+        if (!org) return res.status(404).json({ error: `Org with id ${oid} not found` });
+
+       await Organization.update(
+            validatedBody.data, { where: { id: oid } }
+        );
+
+        const updatedOrganization = await Organization.findByPk(oid);
+        return res.status(200).json({ message: "success", "data": updatedOrganization });
+
+    } catch (err) {
+        res.status(500).json({ updateError: err.message });
+    }
+});
+
+// UPDATE an org's fields
+router.patch('/:oid', async (req, res) => {
+        try {
+        // validate id route param
+        const validatedId = orgParamsValidation.safeParse(req.params);
+        if (!validatedId.success) return res.status(400).json({ paramValidationError: validatedId.error.issues });
+
+        const oid = validatedId.data.oid;
+
+        // validate the request body
+        const validatedBody = orgUpdateValidation.safeParse(req.body);
+        if (!validatedBody.success) return res.status(400).json({ bodyValidationError: validatedBody.error.issues });
+
+        const org = await Organization.findByPk(oid);
+        if (!org) return res.status(404).json({ error: `Org with id ${oid} not found` });
+
+       await Organization.update(
+            validatedBody.data, { where: { id: oid } }
+        );
+
+        const updatedOrganization = await Organization.findByPk(oid);
+        return res.status(200).json({ message: "success", "data": updatedOrganization });
+
+    } catch (err) {
+        res.status(500).json({ updateError: err.message });
+    }
+});
+
+// DELETE an org
+router.delete('/:oid', async (req, res) => {
+    try {
+        // validate oid
+        const validatedId = orgParamsValidation.safeParse(req.params);
+        if (!validatedId.success) return res.status(400).json({ paramValidationError: validatedId.error.issues });
+        const oid = validatedId.data.oid;
+        
+        const deletedCount = await Organization.destroy({
+            where: { id: oid }
+        });
+
+        if (deletedCount === 0) {
+            return res.status(404).json({ error: "Event not found" });
+        }
+
+        res.json({
+            message: "deleted",
+            changes: deletedCount
+        });
+        
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+// GET events by organizations
+router.get('/:oid/events', async (req, res) => {
+    try {
+        const validatedId = orgParamsValidation.safeParse(req.params);
+        if (!validatedId.success) return res.status(400).json({ paramValidationError: validatedId.error.issues });
+        const oid = validatedId.data.oid;
+
+        const events = await Event.findAll({
+            // include: [{
+            //     model: Organization,
+            //     where: { organizationId : oid }
+            // }]
+            where: { organizationId: oid }
+        });
+        if (events.length < 1) {
+            return res.status(404).json({ error: "Events not found" });
+        }
+        res.json({
+            message: "success",
+            data: events,
+        });
+    } catch (err) {
+        res.status(500).json({ findError: err.message });
+    }
+});
+
 
 // CREATE a new event
 router.post('/:oid/events', async (req, res) => {
